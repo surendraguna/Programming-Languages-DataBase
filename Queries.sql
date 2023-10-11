312a6c115924baefeb90c26948a2d2b3c5adf1e0
BOYA SURENDRA <125847917+surendraguna@users.noreply.github.com>
2023-10-11 23:00:40 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -41,3 +41,46 @@ GROUP BY PARADIGM
 ORDER BY LANGUAGE_COUNT DESC
 LIMIT 1;
 
+-- To retrieve a list of programming languages and the integrated development environments (IDEs) associated with them:
+SELECT L.LANGUAGE_NAME, I.IDE_NAME FROM LANGUAGE AS L
+LEFT JOIN IDE AS I ON L.LANGUAGE_ID = I.LANGUAGE_ID
+ORDER BY L.LANGUAGE_NAME;
+
+-- To count the number of programming languages that support each language paradigm:
+SELECT PARADIGM, COUNT(*) AS LANGUAGE_COUNT FROM LANGUAGE
+GROUP BY PARADIGM;
+
+-- To retrieve a list of integrated development environments (IDEs) released after the year 2020:
+SELECT IDE_NAME, RELEASE_DATE FROM IDE
+WHERE RELEASE_DATE > '2020-12-31';
+
+-- To retrieve a list of frameworks released before the year 2010 that are associated with the JavaScript language:
+SELECT FRAMEWORK_NAME, RELEASE_DATE FROM FRAMEWORK
+WHERE LANGUAGE_ID = (SELECT LANGUAGE_ID FROM LANGUAGE WHERE LANGUAGE_NAME = 'JavaScript') AND RELEASE_DATE < '2010-01-01';
+
+-- To find all libraries related to the Java language:
+SELECT LIBRARY_NAME, DESCRIPTION FROM LIBRARY
+WHERE LANGUAGE_ID = 203;
+
+-- To retrieve a list of platforms with the term "Cloud" in their description:
+SELECT PLATFORM_NAME, DESCRIPTION FROM PLATFORM 
+WHERE DESCRIPTION LIKE '%Cloud%';
+
+-- To count how many programming languages were developed in the United States:
+SELECT COUNT(L.LANGUAGE_ID) AS LANGUAGE_COUNT FROM LANGUAGE AS L
+JOIN COUNTRY AS C ON L.COUNTRY_ID = C.COUNTRY_ID
+WHERE C.COUNTRY_NAME = 'United States';
+
+-- To find integrated development environments (IDEs) that support more than one programming language:
+SELECT I.IDE_NAME, COUNT(I.LANGUAGE_ID) AS SUPPORTED_LANGUAGES FROM IDE AS I
+GROUP BY I.IDE_NAME
+HAVING SUPPORTED_LANGUAGES > 1;
+
+-- To retrieve a list of programming languages and their associated paradigms:
+SELECT L.LANGUAGE_NAME, L.PARADIGM
+FROM LANGUAGE AS L
+ORDER BY L.LANGUAGE_NAME;
+
+-- To find frameworks released in the year 2020 or later:
+SELECT FRAMEWORK_NAME, RELEASE_DATE FROM FRAMEWORK
+WHERE RELEASE_DATE >= '2020-01-01';
